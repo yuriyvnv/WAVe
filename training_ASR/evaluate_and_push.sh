e059b10e3e76a11e8107fc5eb137d774e083e62b
yuriyvnv <yuriyvnv@gmail.com>
2025-07-23 23:26:50 +0000

    Evaluation done for model yuriyvnv/whisper-small-cv-only-nl: Wed Jul 23 23:26:50 UTC 2025

--- training_ASR/evaluate_and_push.sh
+++ training_ASR/evaluate_and_push.sh
@@ -9,10 +9,10 @@ uv run ./evaluate_results.py
 echo "=== Pushing logs to GitHub ==="
 git pull
 git add .
-git commit -m "Evaluation done for model yuriyvnv/whisper-large-v3-cv-only-nl: $(date)"
+git commit -m "Evaluation done for model yuriyvnv/whisper-small-cv-only-nl: $(date)"
 git push
 
 
 echo "=== All done! Shutting down instance ==="
-vastai stop instance 23916744 --api-key 8ae7bf55c0e0d706ec35e022dfcc991547b70da038b169380ef994c802f32b43
+vastai stop instance 23906825 --api-key 8ae7bf55c0e0d706ec35e022dfcc991547b70da038b169380ef994c802f32b43
 
