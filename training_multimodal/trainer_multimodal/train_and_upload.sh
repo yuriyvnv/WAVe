f526831f0e739b04b0dc03669823e15001a6ea73
Yuriy Perezhohin <90785024+yuriyvnv@users.noreply.github.com>
2026-02-03 09:24:16 +0000

    update
    Redacted sensitive information in the stop instance command.

--- training_multimodal/trainer_multimodal/train_and_upload.sh
+++ training_multimodal/trainer_multimodal/train_and_upload.sh
@@ -15,5 +15,5 @@ echo "=== Uploading to Hugging Face Hub ==="
 huggingface-cli upload yuriyvnv/3layers_wt_alignment_PT_100_epochs ./3layers_wt_alignment_PT_100_epochs
 
 echo "=== All done! Shutting down instance ==="
-vastai stop instance 30840095  --api-key ac9eb3f9342873e5bdbc3fdff92bb93c254c3f86d79306110877d1933b452ae5
+vastai stop instance redacted_and_destroyed  --api-key redacted_and_deleted
 
